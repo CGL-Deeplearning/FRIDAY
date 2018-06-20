873a5aed40ab5e5a049f45dd94af13a8c0a99d71
Kishwar <kishwar.shafin@gmail.com>
2018-06-20 13:26:49 -0400

    class distribution plot for window size

--- analysis/class_distribution_csv.py
+++ analysis/class_distribution_csv.py
@@ -9,10 +9,14 @@ from collections import OrderedDict
 sns.set(color_codes=True)
 
 file_name = sys.argv[1]
+
 dictionary = {}
 dictionary['0'] = 0
 dictionary['1'] = 0
 dictionary['2'] = 0
+dictionary['3'] = 0
+dictionary['4'] = 0
+dictionary['5'] = 0
 # dictionary['3'] = 0
 with open(file_name, "r") as ins:
     for line in ins:
@@ -20,16 +24,22 @@ with open(file_name, "r") as ins:
         if not line:
             continue
         line = line.split(',')
-        gt = line[2]
+        gt = line[3]
         dictionary[gt] += 1
 
+print('0/0', dictionary['0'])
+print('0/1', dictionary['1'])
+print('1/1', dictionary['2'])
+print('0/2', dictionary['3'])
+print('2/2', dictionary['4'])
+print('1/2', dictionary['5'])
 
 fig, ax = plt.subplots()
 
 dictionary2 = OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
 
 ax.bar(range(len(dictionary2)), dictionary2.values(), align='center')
-plt.xticks(range(len(dictionary2)), ['Hom', 'Het', 'Hom-alt'])
+plt.xticks(range(len(dictionary2)), ['0/0', '0/1', '1/1', '0/2', '2/2', '1/2'])
 
 for i, v in enumerate(dictionary2.values()):
     ax.text(i, v + 500, str(v) + ": " + str(round(v*100/sum(dictionary2.values()), 2)) + "%",
