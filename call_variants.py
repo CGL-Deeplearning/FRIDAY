016d2f183f9150b898f98c49e66141dc245e1c52
Kishwar <kishwar.shafin@gmail.com>
2018-07-09 01:29:31 -0400

    it worked

--- call_variants.py
+++ call_variants.py
@@ -180,19 +180,18 @@ def predict(test_file, batch_size, model_path, gpu_mode, num_workers):
                     if ref_base == '*':
                         continue
 
-                    fake_probs = [0.0] * 6
+                    '''fake_probs = [0.0] * 6
                     fake_probs[true_label] = 1.0
                     top_n, top_i = torch.FloatTensor(fake_probs).topk(1)
                     predicted_label = top_i[0].item()
                     reference_dict[current_genomic_position] = (ref_base, allele_dict_path)
-                    prediction_dict[current_genomic_position].append((predicted_label, fake_probs))
+                    prediction_dict[current_genomic_position].append((predicted_label, fake_probs))'''
 
-                    '''ref_base = reference_seq[seq_index]
                     preds = output_preds[batch, :].data
                     top_n, top_i = preds.topk(1)
                     predicted_label = top_i[0].item()
                     reference_dict[current_genomic_position] = (ref_base, allele_dict_path)
-                    prediction_dict[current_genomic_position].append((predicted_label, preds))'''
+                    prediction_dict[current_genomic_position].append((predicted_label, preds))
 
                     if ref_base != '*':
                         unrolling_genomic_position[batch] += 1
