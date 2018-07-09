fdcf35743897a2056018a8db383d7cf5ed43f1a0
Kishwar <kishwar.shafin@gmail.com>
2018-07-09 00:10:16 -0400

    why the recall so low?

--- call_variants.py
+++ call_variants.py
@@ -175,12 +175,20 @@ def predict(test_file, batch_size, model_path, gpu_mode, num_workers):
                     # current_genomic_position = int(start_positions[batch])
                     current_genomic_position = int(start_positions[batch]) + unrolling_genomic_position[batch]
 
+                    true_label = labels[batch, seq_index]
                     ref_base = reference_seq[seq_index]
+                    fake_probs = [0.0] * 6
+                    fake_probs[true_label] = 1.0
+                    reference_dict[current_genomic_position] = (ref_base, allele_dict_path)
+                    prediction_dict[current_genomic_position].append((true_label, fake_probs))
+
+
+                    '''ref_base = reference_seq[seq_index]
                     preds = output_preds[batch, :].data
                     top_n, top_i = preds.topk(1)
                     predicted_label = top_i[0].item()
                     reference_dict[current_genomic_position] = (ref_base, allele_dict_path)
-                    prediction_dict[current_genomic_position].append((predicted_label, preds))
+                    prediction_dict[current_genomic_position].append((predicted_label, preds))'''
 
                     if reference_seq != '*':
                         unrolling_genomic_position[batch] += 1
