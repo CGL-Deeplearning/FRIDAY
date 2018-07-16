0aed7cc93d1234ccd75fa06c7018f4cc06b3abfb
Kishwar <kishwar.shafin@gmail.com>
2018-07-16 16:52:23 -0700

    class distribution plot changed

--- analysis/class_distribution_csv.py
+++ analysis/class_distribution_csv.py
@@ -24,7 +24,7 @@ with open(file_name, "r") as ins:
         if not line:
             continue
         line = line.split(',')
-        gts = line[3]
+        gts = line[2]
         for gt in gts:
             dictionary[gt] += 1
 
