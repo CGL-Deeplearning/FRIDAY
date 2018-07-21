107ab5425b8e652400d4da15ee68a2b215219410
Kishwar <kishwar.shafin@gmail.com>
2018-07-20 22:25:44 -0700

    shuffle turned off :fire:

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
     train_data_set = SequenceDataset(train_file, transformations)
     train_loader = DataLoader(train_data_set,
                               batch_size=batch_size,
-                              shuffle=True,
+                              shuffle=False,
                               num_workers=num_workers,
                               pin_memory=gpu_mode
                               )
