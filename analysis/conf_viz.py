682cb078585cd30819aa0e6665eab1bfe4402afe
Kishwar <kishwar.shafin@gmail.com>
2018-07-30 02:26:20 -0700

    conf viz updated :boom:

--- analysis/conf_viz.py
+++ analysis/conf_viz.py
@@ -3,12 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 array = \
-[[96173,   371,    10],
- [  103, 46494,   118],
- [    1,   193, 25497]]
+[[1102600,    2794,      67,      41,       0,      15],
+ [   2350,  317869,    1267,      15,       0,     217],
+ [     66,     846,  162988,       0,       0,      84],
+ [    101,      35,       0,     180,       0,      30],
+ [      1,       0,      10,       8,       0,       7],
+ [      8,     123,     114,       7,       0,    4995]]
 array = np.array(array, dtype=float)
 cm = np.array(array, dtype=float)
-n_categories = 3
+n_categories = 6
 for i in range(n_categories):
     array[i] = array[i] / array[i].sum()
 
@@ -27,8 +30,8 @@ print('Recall: ', recall)
 print('F1: ', F1)
 
 sn.set(font_scale=3)
-df_cm = pd.DataFrame(array, index=[i for i in ["Hom", "Het", "Hom-alt"]],
-                  columns=[i for i in ["Hom", "Het", "Hom-alt"]])
+df_cm = pd.DataFrame(array, index=[i for i in ["0/0", "0/1", "1/1", "0/2", "2/2", "1/2"]],
+                  columns=[i for i in ["0/0", "0/1", "1/1", "0/2", "2/2", "1/2"]])
 plt.figure(figsize=(5*4, 4*4))
 
 sn.heatmap(df_cm, annot=True)
