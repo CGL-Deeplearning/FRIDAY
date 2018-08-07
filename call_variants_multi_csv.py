18899309d34e1c40b5d5a763b7768fbfa09759c0
Kishwar <kishwar.shafin@gmail.com>
2018-08-06 22:13:08 -0700

    whole genome prediction

--- call_variants_multi_csv.py
+++ call_variants_multi_csv.py
@@ -319,9 +319,9 @@ def call_variant(csv_file, batch_size, model_path, gpu_mode, num_workers, vcf_di
 
 def call_variants_on_multiple_chromosome(csv_dir, bam_file_path, sample_name, output_dir,
                                          batch_size, model_path, gpu_mode, num_workers, max_threads):
-    chr_list = ["chr20", "chr21", "chr22"]
-    # chr_list = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
-    #             "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
+    # chr_list = ["chr20", "chr21", "chr22"]
+    chr_list = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
+                "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
     # object that can write and handle VCF
     vcf_writer = VCFWriter(bam_file_path, sample_name, output_dir)
 
