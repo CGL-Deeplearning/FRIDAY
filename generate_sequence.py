20e71effecdd77f4db50b02cd6fdd6c0f55aef75
Kishwar <kishwar.shafin@gmail.com>
2018-08-13 20:59:57 -0700

    whole genome image generation :fire:

--- generate_sequence.py
+++ generate_sequence.py
@@ -334,9 +334,9 @@ def genome_level_parallelization(bam_file, ref_file, vcf_file, output_dir_path, 
     """
 
     # --- NEED WORK HERE --- GET THE CHROMOSOME NAMES FROM THE BAM FILE
-    chr_list = ["chr17", "chr18", "chr19"]
-    # chr_list = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
-    #             "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19"]
+    # chr_list = ["chr17", "chr18", "chr19"]
+    chr_list = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
+                "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19"]
 
     program_start_time = time.time()
 
