548fb8cbbe8921f972b13a4407f1329e49e6a437
Kishwar <kishwar.shafin@gmail.com>
2018-08-22 14:40:42 -0700

    context increased to be 20 :fire:

--- modules/core/ImageGenerator.py
+++ modules/core/ImageGenerator.py
@@ -23,7 +23,7 @@ VCF_INDEX_BUFFER = -1
 
 # Per sequence threshold
 # context to take on each side for each base
-CONTEXT_SIZE = 5
+CONTEXT_SIZE = 20
 # jump window size so the last 50 bases will be overlapping
 WINDOW_OVERLAP_JUMP = 10
 # image size
